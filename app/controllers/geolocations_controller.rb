ac4260f99f20d77c18071d3a5941a826e3eb585e
Mike Gwozdek <email": null>
2016-04-24 17:53:12 -0400

    Add determiniation of challenger and creation of battle

--- app/controllers/geolocations_controller.rb
+++ app/controllers/geolocations_controller.rb
@@ -2,8 +2,20 @@ class GeolocationsController < ApplicationController
   def create
     @user = find_user_by_email
     @user_geolocation = Geolocation.create(user: @user, lonlat: make_wkt_point)
-    @challenger = @geolocation.find_closest_challenger
-    @challenger_geolocation = @challenger.geolocations.last
+
+    @challenger = @user_geolocation.find_closest_challenger
+    if @challenger
+      @challenger_geolocation = @challenger.geolocations.last
+
+      # @user.in_battle = true
+      # @challenger.in_battle = true
+
+      @battle = Battle.create(task_id: 1)
+      @battle.users << @user
+      @battle.users << @challenger
+
+      # render 'battles/show', @battle
+    end
   end
 
   private
