6b1d2e035295b23cae5ca43f55d85b2f12be9be1
Mike Gwozdek <email": null>
2016-04-25 18:38:33 -0400

    Delete unused jbuilder templates

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,5 +1,3 @@
 class BattlesController < ApplicationController
-  def show
-    @battle = Battle.includes(:users, :task).find(60)
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    # root 'welcome#index'
   resources :tasks
-  resources :battles, only: :show, defaults: {format: :json}
-  resources :geolocations, only: :create
+  resources :geolocations, only: :create, defaults: {format: :json}
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
