a59524d7b07cc4881f4a2bb26f4c036f2c009196
Case104 <jpcase104@gmail.com>
2016-04-26 17:11:22 -0400

    Adding loser to schema and routes to battlground to support battleprompt. Navigate to task inplimentated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
   resources :geolocations, only: :create, defaults: {format: :json}
-  post 'accept_deny' => 'battle#accept_deny'
   resources :users, only: [:create, :show]
+	patch '/battle/decline', to: 'battles#decline'
 end
+
