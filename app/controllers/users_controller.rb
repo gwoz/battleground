73e1d6a13cec4086da4f464f889b9e0f809eea46
Case104 <jpcase104@gmail.com>
2016-04-26 12:47:37 -0400

    Fixing redirect warning. Returning user as json to front end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 	def create
 		user_data = JSON.parse(request.body.string)
 		@user = User.find_or_create_by(email: user_data['email'])
-		redirect_to '/'
+		render json: @user
 	end
 
 	def show
