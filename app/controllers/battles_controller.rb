18adbf69d183000243f28a02fe16fb0298582b6e
Mike Gwozdek <email": null>
2016-04-26 08:02:11 -0400

    Add accept/deny route to handle battle prompt

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,3 +1,32 @@
 class BattlesController < ApplicationController
 
+  def accept_deny
+    @user = find_user_by_email
+    @challegner = find_challenger_by_email
+    @battle = find_battle_by_id
+    user_response = find_user_response
+
+    if user_response == 'decline'
+      @user.update(in_battle: false)
+      @battle.update(winner: @challenger)
+    elsif user_response == 'accept'
+      @user.update(in_battle: false)
+    end
+    render nothing: true
+  end
+
+  private
+
+  def find_user_by_email
+  end
+
+  def find_challenger_by_email
+  end
+
+  def find_battle_by_id
+  end
+
+  def find_user_response
+  end
+
 end
--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,8 +1,25 @@
+ACCEPT_BATTLE_POINTS = 5
+DECLINE_BATTLE_POINTS = -5
+WIN_BATTLE_POINTS = 25
+
 class Battle < ActiveRecord::Base
 	has_and_belongs_to_many :users
 
   has_many :user_locations
   belongs_to :task
+
+  def accept_decline_points(response)
+    case response
+    when 'accept'
+      ACCEPT_BATTLE_POINTS
+    when 'decline'
+      DECLINE_BATTLE_POINTS
+    end
+  end
+
+  def change_battle_score(user,points)
+  end
+
 end
 
 
