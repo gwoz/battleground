96a63a8337c987db28c6c65565ecc73d6a77a7b8
Case104 <jpcase104@gmail.com>
2016-04-27 10:32:14 -0400

    Logic for totalling points

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ActiveRecord::Base
 
   validates :email, uniqueness: true, presence: true
   has_many :geolocations
+
+  def total_points
+  	get_win_points - get_loss_points
+  end
+
+  private
+
+  WIN_VALUE = 15
+  LOSS_VALUE = 5
+
+  def get_win_points
+  	self.battles.where(winner: self.id).count * WIN_VALUE
+  end
+
+  def get_loss_points
+  	self.battles.where(loser: self.id).count * LOSS_VALUE
+  end
+  
 end
