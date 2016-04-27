9ca496319938b16d81cbf772077918ef0010e8ed
Case104 <jpcase104@gmail.com>
2016-04-27 18:10:59 -0400

    google and facebook integration implimented.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
 
+	def index
+		@users = User.all.order(points: :desc)
+		render json: @users
+	end
+
 	def create
 		user_data = JSON.parse(request.body.string)
-		@user = User.find_or_create_by(email: user_data['email'], avatar: user_data['photo'])
-		render json: @user
+		@user = User.find_by(email: user_data['email'])
+		if @user 
+			render json: @user
+		else
+			@user = User.create(email: user_data['email'], avatar: user_data['photo'])
+			render json: @user
+		end
 	end
 
 	def show
